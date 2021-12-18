b07e2e00a1be6c7cf2410547a6809c2101a71ce0
Marian-Janolek <m.janolek@gmail.com>
2021-12-18 19:25:08 +0100

    add footer services and testimonials

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,40 @@
+import styles from '../styles/Footer.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
+
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.cardL}>
+        <h1 className={styles.title}>AVOCAD0 CREATIVES.</h1>
+        <h1 className={styles.linkTitle}>
+          <a href="/contact" passHref>
+            <span className={styles.linkText}>WORK WITH US</span>
+            <Image src="/assets/link.png" alt="" width="40px" height="40px" />
+          </a>
+        </h1>
+      </div>
+      <div className={styles.cardS}>
+        <div className={styles.cardItem}>
+          12 ADAM STREET NY <br /> USA
+        </div>
+        <div className={styles.cardItem}>
+          CONTACT@M.J <br /> + 123 223 22 88
+        </div>
+      </div>
+      <div className={styles.cardS}>
+        <div className={styles.cardItem}>
+          FOLLOW US:
+          <br /> __FB __IN __BE __TW
+        </div>
+        <div className={styles.cardItem}>
+          © 2022 MJ INTERACTIVE,
+          <br />
+          ALL RIGHTS RESERVED
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
---
+++ components/Services.jsx
@@ -0,0 +1,43 @@
+import styles from '../styles/Services.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
+
+const Services = ({ services }) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>What We Can Do?</h1>
+      <h1 className={styles.subTitle}>Services we can help you with</h1>
+      <div className={styles.services}>
+        {services.map((service) => (
+          <Link passHref href={`/products/${service.name}`} key={service.id}>
+            <div className={styles.service}>
+              <div className={styles.desc}>{service.desc}</div>
+              <span className={styles.cat}>{service.title}</span>
+              <div className={styles.media}>
+                {service.video ? (
+                  <video
+                    src={`/assets/${service.video}`}
+                    autoPlay
+                    loop
+                    className={styles.video}
+                  />
+                ) : (
+                  <Image
+                    alt=""
+                    src={`/assets/${service.photo}`}
+                    width="100%"
+                    height="100%"
+                    layout="responsive"
+                    objectFit="cover"
+                  />
+                )}
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Services;
